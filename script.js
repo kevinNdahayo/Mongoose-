2b70ca70826aa53a6cdc126ff8243688d7088391
kevinNdahayo <ndahayokevin1@gmail.com>
2022-02-25 18:41:02 -0800

    defined schema and model

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log("Mongoose")
+const mongoose = require("mongoose");
+const mongodbUrl = "mongodb+srv://kevin:ndahayokevin2003@mongodb@cluster0.7fss6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const User = require("./user")
+mongoose.connect(mongodbUrl, () => {
+    console.log("Connected successfully"),
+    error => {
+        console.error(error);
+    }
+})
+run()
+async function run(){
+    User.create()
+    const user1 = new User({
+    name: "Kevin",
+    age: 26
+})
+  await user1.save()
+  console.log(user1)
+}
